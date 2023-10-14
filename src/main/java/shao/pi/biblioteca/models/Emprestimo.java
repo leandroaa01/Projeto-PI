ec3cfd510a7e18b8c06b3c1480af73e9cdc8fc40
Leandro Andrade <leandro.andrade.araujo00@gmail.com>
2023-10-14 12:54:43 -0300

    Criação das entidades(banco de dados). Obs: Falta os controllers

--- src/main/java/shao/pi/biblioteca/models/Emprestimo.java
+++ src/main/java/shao/pi/biblioteca/models/Emprestimo.java
@@ -1,5 +1,97 @@
 package shao.pi.biblioteca.models;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Emprestimo {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String tituloLivro;
+
+    @NotBlank
+    private Long matriculaAluno;
     
+    @NotBlank
+    private String situacao;
+    
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+    private  LocalDate dEmprestimo;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+    private  LocalDate dDevolucao;
+   
+    @PrePersist
+    public void prePersist() {
+    this.dEmprestimo = LocalDate.now();
+    this.dDevolucao = this.dEmprestimo.plusDays(14);
 }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTituloLivro() {
+        return tituloLivro;
+    }
+
+    public void setTituloLivro(String tituloLivro) {
+        this.tituloLivro = tituloLivro;
+    }
+
+    public Long getMatriculaAluno() {
+        return matriculaAluno;
+    }
+
+    public void setMatriculaAluno(Long matriculaAluno) {
+        this.matriculaAluno = matriculaAluno;
+    }
+
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public LocalDate getdEmprestimo() {
+        return dEmprestimo;
+    }
+
+    public void setdEmprestimo(LocalDate dEmprestimo) {
+        this.dEmprestimo = dEmprestimo;
+    }
+
+    public LocalDate getdDevolucao() {
+        return dDevolucao;
+    }
+
+    public void setdDevolucao(LocalDate dDevolucao) {
+        this.dDevolucao = dDevolucao;
+    }
+
+    @Override
+    public String toString() {
+        return "Emprestimo [id=" + id + ", tituloLivro=" + tituloLivro + ", matriculaAluno=" + matriculaAluno
+                + ", situacao=" + situacao + ", dEmprestimo=" + dEmprestimo + ", dDevolucao=" + dDevolucao + "]";
+    }
+}
+
+
--- src/main/java/shao/pi/biblioteca/models/Livro.java
+++ src/main/java/shao/pi/biblioteca/models/Livro.java
@@ -1,5 +1,78 @@
 package shao.pi.biblioteca.models;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String titulo;
+
+    @NotBlank
+    private String autor;
     
+    @NotNull
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+    private  LocalDate anoPublicado; 
+
+    @NotBlank
+    private String edicao;
+
+    @Override
+    public String toString() {
+        return "Livro [id=" + id + ", titulo=" + titulo + ", autor=" + autor + ", anoPublicado=" + anoPublicado
+                + ", edicao=" + edicao + "]";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getAnoPublicado() {
+        return anoPublicado;
+    }
+
+    public void setAnoPublicado(LocalDate anoPublicado) {
+        this.anoPublicado = anoPublicado;
+    }
+
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
 }
